e4fc73d187c915962673c64bb1a1d27eee7133c1
Isamu <poyisamu@gmail.com>
2025-07-24 23:30:49 -0700

    fixing sign ins so that the token is properly accessed right after signing in, and also implementing sign out such that when a user signs out, the credentials are no longer valid and removed. The board is only clickable if the user is signed in. Additionally, removing logout redirect because we want to stay on the same page instead of a logout page, and removing the signout functions

--- frontend/cognitoManager.js
+++ frontend/cognitoManager.js
@@ -4,9 +4,7 @@ import { UserManager } from "https://cdn.skypack.dev/oidc-client-ts";
 const cognitoDomain = "https://us-east-2poklryknt.auth.us-east-2.amazoncognito.com";
 const clientId = "7uqje135h2m1tu0t1j2bdasgq3";
 
-// Replace with your actual S3 static site URL
 const redirectUri = "https://d3j7mmciz70vi1.cloudfront.net/callback.html";
-const logoutUri = "https://d3j7mmciz70vi1.cloudfront.net/";
 
 export const userManager = new UserManager({
     authority: cognitoDomain,
@@ -23,7 +21,3 @@ export const userManager = new UserManager({
         end_session_endpoint: `${cognitoDomain}/logout`,
     }
 });
-
-export function signOutRedirect() {
-    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
-}
