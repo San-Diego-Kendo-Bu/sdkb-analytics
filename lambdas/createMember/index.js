0e7b6b2c2e88c8e1aec8daa9b8746b3ab1f32cab
Isamu <poyisamu@gmail.com>
2025-08-03 19:56:06 -0700

    import QueryCommand

--- lambdas/createMember/index.js
+++ lambdas/createMember/index.js
@@ -1,4 +1,4 @@
-const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBClient, QueryCommand } = require("@aws-sdk/client-dynamodb");
 const {
   DynamoDBDocumentClient,
   PutCommand,
