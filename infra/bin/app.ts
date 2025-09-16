c29e4e6b1b7e4360544bff16e8ee4568ecc534cd
Ben Lonc <bnlonc@gmail.com>
2025-09-15 23:44:55 -0700

    Overhauled CDK

---
+++ infra/bin/app.ts
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+import { ComputeStack } from '../lib/stacks/compute-stack';
+import { App } from 'aws-cdk-lib';
+import { STAGES } from '../lib/constants/stages';
+import { StorageStack } from '../lib/stacks/storage-stack';
+import { IamStack } from '../lib/stacks/iam-stack';
+import { IdentityStack } from '../lib/stacks/identity-stack';
+import { InputStack } from '../lib/stacks/input-stack';
+import { SecretsStack } from '../lib/stacks/secrets-stack';
+
+const app = new App();
+
+STAGES.forEach((stage) => {
+  const secretsStack = new SecretsStack(app, `SecretsStack-${stage.name}-${stage.env.region}`, {
+    env: stage.env,
+  });
+
+  const identityStack = new IdentityStack(app, `IdentityStack-${stage.name}-${stage.env.region}`, {
+    env: stage.env,
+    userPoolId: stage.userPoolId,
+    userPoolClientId: stage.userPoolClientId,
+    stageName: stage.name,
+  });
+
+  const computeStack = new ComputeStack(app, `ComputeStack-${stage.name}-${stage.env.region}`, {
+    env: stage.env,
+    membersAuthorizer: identityStack.membersAuthorizer,
+    stripeSecretName: secretsStack.stripeSecret.secretName,
+  });
+
+  const storageStack = new StorageStack(app, `StorageStack-${stage.name}-${stage.env.region}`, {
+    env: stage.env,
+  });
+
+  const iamStack = new IamStack(app, `IamStack-${stage.name}-${stage.env.region}`, {
+    env: stage.env,
+    stripeSecret: secretsStack.stripeSecret,
+    lambdaFunctions: computeStack.lambdaFunctions,
+    membersTableArn: stage.membersTableArn,
+    configTableArn: stage.configTableArn,
+  });
+
+  const inputStack = new InputStack(app, `InputStack-${stage.name}-${stage.env.region}`, {
+    env: stage.env,
+    membersAuthorizer: identityStack.membersAuthorizer,
+    lambdaFunctions: computeStack.lambdaFunctions,
+  });
+});
---
+++ infra/lib/stacks/compute-stack.ts
@@ -0,0 +1,75 @@
+import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import { IHttpRouteAuthorizer } from "aws-cdk-lib/aws-apigatewayv2";
+import { Code, Function, IFunction, Runtime } from "aws-cdk-lib/aws-lambda";
+import { LogLevel, NodejsFunction, OutputFormat } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+import path from "path";
+
+export interface ComputeStackProps extends StackProps {
+  membersAuthorizer: IHttpRouteAuthorizer;
+  stripeSecretName: string;
+}
+
+export interface NafudakakeLambdaFunctions {
+  createMemberLambda: IFunction;
+  getMembersLambda: IFunction;
+  getAdminLambda: IFunction;
+  removeMemberLambda: IFunction;
+  modifyMemberLambda: IFunction;
+}
+
+export class ComputeStack extends Stack {
+
+  public readonly lambdaFunctions: NafudakakeLambdaFunctions;
+
+  constructor(scope: Construct, id: string, props: ComputeStackProps) {
+    super(scope, id, props);
+
+    this.lambdaFunctions = {
+      createMemberLambda: new NodejsFunction(this, "CreateMemberLambda", {
+        functionName: "CreateMemberLambda",
+        entry: path.join(__dirname, "../../lambdas/members/createMember/index.js"),
+        handler: "handler",
+        runtime: Runtime.NODEJS_18_X,
+        timeout: Duration.seconds(10),
+        projectRoot: path.join(__dirname, "../../"),
+        depsLockFilePath: path.join(__dirname, "../../package-lock.json"),
+        bundling: {
+          target: "node18",
+          format: OutputFormat.CJS,
+          externalModules: [],
+          minify: true,
+          sourceMap: true,
+          logLevel: LogLevel.DEBUG,
+        },
+        environment: {
+          SECRET_ID: props.stripeSecretName,
+        },
+      }),
+      getMembersLambda: new Function(this, 'GetMembersLambda', {
+        functionName: 'GetMembersLambda',
+        runtime: Runtime.NODEJS_18_X,
+        handler: 'index.handler',
+        code: Code.fromAsset(path.join(__dirname, '../../lambdas/members/getMembers')),
+      }),
+      getAdminLambda: new Function(this, 'GetAdminLambda', {
+        functionName: 'GetAdminLambda',
+        runtime: Runtime.NODEJS_18_X,
+        handler: 'index.handler',
+        code: Code.fromAsset(path.join(__dirname, '../../lambdas/admins/getAdmin')),
+      }),
+      removeMemberLambda: new Function(this, 'RemoveMemberLambda', {
+        functionName: 'RemoveMemberLambda',
+        runtime: Runtime.NODEJS_18_X,
+        handler: 'index.handler',
+        code: Code.fromAsset(path.join(__dirname, '../../lambdas/members/removeMember')),
+      }),
+      modifyMemberLambda: new Function(this, 'ModifyMemberLambda', {
+        functionName: 'ModifyMemberLambda',
+        runtime: Runtime.NODEJS_18_X,
+        handler: 'index.handler',
+        code: Code.fromAsset(path.join(__dirname, '../../lambdas/members/modifyMember')),
+      }),
+    }
+  }
+}
