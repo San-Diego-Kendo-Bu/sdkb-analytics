6e697612b848539daffe869a535ef7e14a8a636a
Isamu <poyisamu@gmail.com>
2025-10-08 20:30:23 -0700

    add event creation lambda

---
+++ infra/lambdas/events/createEvent/index.js
@@ -0,0 +1,100 @@
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const {
+  DynamoDBDocumentClient, UpdateCommand,
+} = require("@aws-sdk/lib-dynamodb");
+
+const { createClient } = require("@supabase/supabase-js");
+const ENDPOINT = 'https://gsriiicvvxzvidaakctw.supabase.co';
+const EVENTS_TABLE = "Events";
+
+const { SecretsManagerClient, GetSecretValueCommand } = require("@aws-sdk/client-secrets-manager");
+const REGION = process.env.AWS_REGION;
+const SUPABASE_SECRET_ID = process.env.SUPABASE_SECRET_ID;
+const secrets_client = new SecretsManagerClient({ region: REGION });
+const ddb = DynamoDBDocumentClient.from(new DynamoDBClient({ region: REGION }));
+
+function dummyCognito(){
+    return ['admin@gmail.com'];
+}
+
+function isAdmin(clientEmail){
+    return dummyCognito()[0] === clientEmail;
+}
+
+async function getSupabase(){
+    const r = await secrets_client.send(new GetSecretValueCommand({ SecretId: SUPABASE_SECRET_ID }));
+    const raw = r.SecretString ?? Buffer.from(r.SecretBinary || "", "base64").toString("utf8");
+    const obj = JSON.parse(raw); 
+    const api_key = obj.SUPABASE_SECRET_KEY;
+    const cachedSupabase = createClient(ENDPOINT, api_key);
+    return cachedSupabase;
+}
+
+exports.handler = async (event) => {
+
+    const clientEmail = event.headers["client_email"];
+    
+    if(!isAdmin(clientEmail))
+        return { statusCode: 403, body: "Forbidden" };
+
+    try {
+        const updateParams = {
+            TableName: "appConfigs",
+            Key: { type: "eventIdCounter" },
+            UpdateExpression: "ADD #counter :val",
+            ExpressionAttributeNames: { "#counter": "idCounter" },
+            ExpressionAttributeValues: { ":val": 1 },
+            ReturnValues: "ALL_NEW",
+        };
+        const updateResult = await ddb.send(new UpdateCommand(updateParams));
+        const newEventId = updateResult.Attributes.idCounter;
+
+        const parameters = JSON.parse(event.body);
+
+        const eventName = parameters.event_name;
+        const eventType = parameters.event_type;
+        const createdAt = parameters.created_at;
+        const eventDate = parameters.event_date;
+        const eventDeadline = parameters.event_deadline;
+        const eventLocation = parameters.event_location;
+
+        const supabase = await getSupabase();
+
+        const response = await supabase.from(EVENTS_TABLE).insert({
+            event_id: newEventId,
+            event_date: eventDate,
+            event_name: eventName,
+            event_type: eventType,
+            event_deadline: eventDeadline,
+            created_at: createdAt,
+            event_location: eventLocation
+        });
+        
+        if(response.error){
+            return {
+                statusCode: 500,
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ error: response.error })
+            }; 
+        }
+
+        return{
+            statusCode : 200,
+            headers : {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            },
+            body : JSON.stringify({
+                message: "Created Event Successfully",
+                request_parameters: parameters,
+            })
+        };
+
+    } catch (err) {
+        return {
+            statusCode: 500,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ error: err.message })
+        };
+    }
+};
