24c8e983c724cd28985943e2953652dbe22cef34
Isamu <poyisamu@gmail.com>
2025-10-08 22:03:30 -0700

    add remove event lambda

---
+++ infra/lambdas/events/removeEvent/index.js
@@ -0,0 +1,63 @@
+const { getSupabase } = require("../../shared_utils/supabase");
+
+const SUPABASE_SECRET_ID = process.env.SUPABASE_SECRET_ID;
+const REGION = process.env.AWS_REGION;
+const EVENTS_TABLE = "Events";
+
+function dummyCognito(){
+    return ['admin@gmail.com'];
+}
+
+function isAdmin(clientEmail){
+    return dummyCognito()[0] === clientEmail;
+}
+
+exports.handler = async (event) => {
+
+    const clientEmail = event.headers["client_email"];
+    
+    if(!isAdmin(clientEmail))
+        return { statusCode: 403, body: "Forbidden" };
+
+    try {
+        
+        const parameters = JSON.parse(event.body);
+        const eventId = parameters.event_id;
+        
+        if(!eventId){
+            return{
+                status: 400,
+                message: "Please specify a event id"
+            }
+        }
+
+        const supabase = await getSupabase(SUPABASE_SECRET_ID, REGION);
+        const response = await supabase.from(EVENTS_TABLE).delete().eq('event_id', eventId);
+
+        if(response.error){
+            return{
+                statusCode: 500,
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ error: response.error })
+            };
+        }
+        
+        return{
+            statusCode : 200,
+            headers : {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            },
+            body : JSON.stringify({
+                message: "Deleted Event Successfully",
+            })
+        };
+
+    } catch (err) {
+        return {
+            statusCode: 500,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ error: err.message })
+        };
+    }
+};
