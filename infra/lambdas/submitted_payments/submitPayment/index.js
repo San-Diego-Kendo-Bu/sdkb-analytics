5e2c207da6df48b957e7ee0800ae142514fa4ccc
Gabriel Ikezaki <gabriel.ikezaki@gmail.com>
2025-11-06 15:01:20 -0800

    Refactored: submitPayment now calls PostgreSQL function
    Instead of making 3 separate calls to Supabase, it makes one call to a submit_payment function that retrieves/deletes from AssignedPayment and inserts into SubmitPayment

--- infra/lambdas/submitted_payments/submitPayment/index.js
+++ infra/lambdas/submitted_payments/submitPayment/index.js
@@ -2,12 +2,7 @@ const { getCurrentTimeUTC } = require("../../shared_utils/dates");
 const { verifyMemberExists } = require("../../shared_utils/members.js");
 const { getSupabase } = require("../../shared_utils/supabase");
 
-const PAYMENTS_TABLE = "Payments";
-const ASSIGNED_PAYMENTS_TABLE = "AssignedPayments";
-
 const REQUIRED_FIELDS = ["member_id", "payment_id"];
-
-const SUBMITTED_TABLE = "SubmittedPayments";
 
 const SUPABASE_SECRET_ID = process.env.SUPABASE_SECRET_ID;
 const REGION = process.env.AWS_REGION;
@@ -46,77 +41,24 @@ exports.handler = async (event) => {
             return { statusCode: 400, body: "Invalid member ID." };
         }
 
+        const submittedOn = parameters['submitted_on'] ? parameters['submitted_on'] : getCurrentTimeUTC();
+
         const supabase = await getSupabase(SUPABASE_SECRET_ID, REGION);
         
-        // 1. Look up AssignedPayments w/ member ID + payment ID. If no entry is found, return
-        // 2. Store: payment_id, assigned_on, status.
-        const assignedPaymentResponse = 
-            await supabase.from(ASSIGNED_PAYMENTS_TABLE)
-            .select()
-            .match(ids);
-        if(assignedPaymentResponse.error){
-            return {
-                statusCode: 500,
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ error: response.error })
-            }; 
-        }
-
-        const assignedEntry = assignedPaymentResponse.data[0];
-        const paymentId = parseInt(assignedEntry.payment_id);
-        const assignedOn = assignedEntry.assigned_on;
-        const status = assignedEntry.status;
-
-        // 3. Look up at Payments w/ payment_id.
-        const paymentResponse = await supabase.from(PAYMENTS_TABLE).select().eq('payment_id', paymentId);
-        if(paymentResponse.error){
-            return{
-                statusCode: 500,
-                headers: { "Content-Type" : "application/json" },
-                body: JSON.stringify( {error:paymentResponse.error} )
-            };
-        }
-
-        const paymentEntry = paymentResponse.data[0];
-        // 4. Store: payment_value, overdue_penalty.
-        const paymentValue = (paymentEntry["payment_value"]) ? parseFloat(paymentEntry["payment_value"]) : 0.00;
-        const overdue = (status === "overdue");
-        const overdueValue = (paymentEntry["overdue_penalty"] && overdue) ? parseFloat(paymentEntry["overdue_penalty"]) : 0.00;
-        const totalPaid = paymentValue + overdueValue;
-        const submittedOn = parameters["submitted_on"] ? parameters["submitted_on"] : getCurrentTimeUTC();
-        
-        // 5. Create new Submission
-        const payload = {
-            payment_id : paymentId, 
-            member_id : memberId, 
-            assigned_on : assignedOn,
-            submitted_on : submittedOn,
-            overdue : overdue,
-            total_paid : totalPaid
-        }
-
-        const response = await supabase.from(SUBMITTED_TABLE).insert(payload).select();
+        const response = await supabase.rpc('submit_payment', {
+            p_member_id : ids.member_id, 
+            p_payment_id : ids.payment_id,
+            p_submitted_on : submittedOn
+        }); 
         if(response.error){
-            return {
-                statusCode: 500,
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ error: response.error })
-            }; 
-        }
-        
-        // 6. Delete entry from assigned table only if submitTable insertion (and operations prior) was successful
-        const removeAssignedEntry = await supabase.from(ASSIGNED_PAYMENTS_TABLE)
-            .delete()
-            .match(ids);
-        if(removeAssignedEntry.error){
             return{
                 statusCode: 500,
-                headers: {"Content-Type" : "application/json"},
-                body: JSON.stringify({error : removeAssignedEntry.error})
-            };
+                headers : {"Content-Type" : "application/json"},
+                body: JSON.stringify({error:response.error})
+            }
         }
-
-        const data = response.data[0];
+        
+        const data = response.data;
         return{
             statusCode : 200,
             headers : {"Content-Type" : "application/json"},
