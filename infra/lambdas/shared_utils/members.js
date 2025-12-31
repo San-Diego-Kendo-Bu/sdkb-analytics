17d48cea5aa985de543ecb90c4e6ba26fec14ce3
isamumu <isamu510@hotmail.com>
2025-12-31 20:22:10 +0900

    add function to extract username from claims

--- infra/lambdas/shared_utils/members.js
+++ infra/lambdas/shared_utils/members.js
@@ -1,19 +1,19 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const {
-    QueryCommand,
-    ScanCommand,
-    DynamoDBDocumentClient
+  QueryCommand,
+  ScanCommand,
+  DynamoDBDocumentClient
 } = require("@aws-sdk/lib-dynamodb");
 const { get } = require("http");
 
 const REGION = process.env.AWS_REGION;
-const ddb = DynamoDBDocumentClient.from(new DynamoDBClient({ region : REGION }));
+const ddb = DynamoDBDocumentClient.from(new DynamoDBClient({ region: REGION }));
 
 const MEMBER_ID_ATTR = "member_id";
-const MEMBERS_TABLE  = "members";
+const MEMBERS_TABLE = "members";
 
-const EMAIL_ATTR     = "email";
-const EMAIL_INDEX    = "email-index";
+const EMAIL_ATTR = "email";
+const EMAIL_INDEX = "email-index";
 
 async function getMemberById(memberId) {
   const responseItems = [];
@@ -22,8 +22,8 @@ async function getMemberById(memberId) {
     const response = await ddb.send(new QueryCommand({
       TableName: MEMBERS_TABLE,
       KeyConditionExpression: "#e = :e",
-      ExpressionAttributeNames:  { "#e": MEMBER_ID_ATTR },
-      ExpressionAttributeValues: { ":e": memberId }, 
+      ExpressionAttributeNames: { "#e": MEMBER_ID_ATTR },
+      ExpressionAttributeValues: { ":e": memberId },
       ExclusiveStartKey: lastKey,
     }));
     responseItems.push(...(response.Items || []));
@@ -42,7 +42,7 @@ async function getMembersByEmail(email) {
       TableName: MEMBERS_TABLE,
       IndexName: EMAIL_INDEX,
       KeyConditionExpression: "#e = :e",
-      ExpressionAttributeNames:  { "#e": EMAIL_ATTR },
+      ExpressionAttributeNames: { "#e": EMAIL_ATTR },
       ExpressionAttributeValues: { ":e": email },
       ExclusiveStartKey: lastKey,
     }));
@@ -54,47 +54,53 @@ async function getMembersByEmail(email) {
 }
 
 async function verifyMemberExists(memberId) {
-    let lastKey;
-    const response = await ddb.send(new QueryCommand({
-        TableName: MEMBERS_TABLE,
-        KeyConditionExpression: "#e = :e",
-        ExpressionAttributeNames:  { "#e": MEMBER_ID_ATTR },
-        ExpressionAttributeValues: { ":e": memberId },
-    }));
+  let lastKey;
+  const response = await ddb.send(new QueryCommand({
+    TableName: MEMBERS_TABLE,
+    KeyConditionExpression: "#e = :e",
+    ExpressionAttributeNames: { "#e": MEMBER_ID_ATTR },
+    ExpressionAttributeValues: { ":e": memberId },
+  }));
 
-    if(response.Items.length === 0)
-        return false;
+  if (response.Items.length === 0)
+    return false;
 
   return true;
 }
 
 async function getAllMembers() {
-    const response = await ddb.send(new ScanCommand({
-      TableName: MEMBERS_TABLE,
-    }));
-    return response.Items || [];
+  const response = await ddb.send(new ScanCommand({
+    TableName: MEMBERS_TABLE,
+  }));
+  return response.Items || [];
 }
 
 async function getAllMemberIds(onlyActiveNoExempt, noGuests, onlySenseis) {
-    const allMembers = await getAllMembers();
-    // Status types: exempt, inactive, guest 
-    // if onlyActiveNoExempt, filter out exempt and inactive
-    // if noGuests, filter out guest
-    // if onlySenseis, only include senseis (exempt status members)
+  const allMembers = await getAllMembers();
+  // Status types: exempt, inactive, guest 
+  // if onlyActiveNoExempt, filter out exempt and inactive
+  // if noGuests, filter out guest
+  // if onlySenseis, only include senseis (exempt status members)
 
-    // Apply filters
-    let filteredMembers = allMembers;
-    if(onlyActiveNoExempt){
-        filteredMembers = filteredMembers.filter(member => member.status !== "exempt" && member.status === "active");
-    }
-    if(noGuests){
-        filteredMembers = filteredMembers.filter(member => member.status !== "guest");
-    }
-    if(onlySenseis){
-        filteredMembers = filteredMembers.filter(member => member.status === "exempt");
-    }
+  // Apply filters
+  let filteredMembers = allMembers;
+  if (onlyActiveNoExempt) {
+    filteredMembers = filteredMembers.filter(member => member.status !== "exempt" && member.status === "active");
+  }
+  if (noGuests) {
+    filteredMembers = filteredMembers.filter(member => member.status !== "guest");
+  }
+  if (onlySenseis) {
+    filteredMembers = filteredMembers.filter(member => member.status === "exempt");
+  }
 
-    return filteredMembers.map(member => member.member_id);
+  return filteredMembers.map(member => member.member_id);
 }
 
-module.exports = { getMemberById, getMembersByEmail, verifyMemberExists , getAllMemberIds };
+async function getMemberIdByToken(claims) {
+  const username = claims["cognito:username"];
+
+  return username;
+}
+
+module.exports = { getMemberById, getMembersByEmail, verifyMemberExists, getAllMemberIds, getMemberIdByToken };
